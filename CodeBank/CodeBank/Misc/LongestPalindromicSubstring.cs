2a08c0b8f8ff0026d9285b3ddb3be73462788c98
agarwalneha01 <agarwalneha.cs@gmail.com>
2017-04-15 16:16:26 -0700

    longest palindrome

---
+++ CodeBank/CodeBank/Misc/LongestPalindromicSubstring.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CodeBank.Misc
+{
+    public class LongestPalindromicSubstring
+    {
+        public static string LongestPalindromicSubstring_String(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 1)
+            {
+                return s;
+            }
+
+            var data = new int[s.Length];
+            data[0] = 1;
+            int centerIndex = 0;
+
+            //Del with odd length
+            for (var i = 1; i < s.Length; i++)
+            {
+                data[i] = 1;
+                var begin = i - 1;
+                var end = i + 1;
+                while (begin >= 0 && end < s.Length && s[begin] == s[end])
+                {
+                    data[i] += 2;
+                    centerIndex = (data[centerIndex] < data[i]) ? i : centerIndex;
+                    begin--;
+                    end++;
+                }
+            }
+
+            //Then with even lengths
+            for (var i = 1; i < s.Length; i++)
+            {
+                if(s[i] == s[i-1])
+                {
+                    var currLen = 2;
+                    var end = i + 1;
+                    var begin = i - 2;
+                    while (begin >= 0 && end < s.Length && s[begin] == s[end])
+                    {
+                        currLen += 2;
+                        begin--;
+                        end++;
+                    }
+                    data[i] = Math.Max(currLen, data[i]);
+                    centerIndex = (data[centerIndex] < data[i]) ? i : centerIndex;
+                }
+            }
+
+            return GetSubString(s, data, data[centerIndex]);
+        }
+
+        private static string GetSubString(string s, int[] data, int maxLength)
+        {
+            int i = 0;
+            for (; i < data.Length; i++)
+            {
+                if (data[i] == maxLength)
+                {
+                    break;
+                }
+            }
+            return s.Substring(i - data[i] / 2, data[i]);
+        }
+    }
+}
---
+++ CodeBank/CodeBankTests/Misc/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBank.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBank.Misc.Tests
+{
+    [TestClass()]
+    public class LongestPalindromicSubstringTests
+    {
+        [TestMethod()]
+        public void LongestPalindromicSubstring_StringTest()
+        {
+            var str = "aaaabaaa";
+
+            var longest = LongestPalindromicSubstring.LongestPalindromicSubstring_String(str);
+            var x = 1;
+        }
+    }
+}
